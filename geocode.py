03b2a19cbaae96f4474227295fe03be6d146aa56
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-03-29 08:21:21 +0600

    Update the returned result (return only attitude and longitude)

--- geocode.py
+++ geocode.py
@@ -2,11 +2,14 @@ import httplib2
 import json
 
 
+google_api_key = 'AIzaSyCQe4BASdsAFJ1yWLARuKuHL2uuJzQ3GhM'
+
+
 def getGeocodeLocation(placeName):
-    google_api_key = 'AIzaSyCQe4BASdsAFJ1yWLARuKuHL2uuJzQ3GhM'
     locationString = placeName.replace(' ', '+')
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (locationString, google_api_key))
     h = httplib2.Http()
-    response, content = h.request(url, 'GET')
-    result = json.loads(content)
-    return result
+    result = json.loads(h.request(url, 'GET')[1])
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return latitude, longitude
