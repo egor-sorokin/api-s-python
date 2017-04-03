ef4090dc8d29aae95fe5e0e35b8c46fa02b01e7d
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-04-03 07:57:22 +0600

    Add get and post methods of restaurants

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,7 +4,7 @@ from models import Base, Restaurant
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from findRestaurant import find_restaurant
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
@@ -21,9 +21,24 @@ app = Flask(__name__)
 @app.route('/restaurants/', methods=['GET', 'POST'], strict_slashes=False)
 def all_restaurants_handler():
     if request.method == 'GET':
-        pass
+        restaurants = session.query(Restaurant).all()
+        return jsonify(restaurants=[i.serialize for i in restaurants])
+
     elif request.method == 'POST':
-        pass
+        location = request.args.get('location', '')
+        meal_type = request.args.get('mealType', '')
+
+        restaurant_info = find_restaurant(meal_type, location)
+        if restaurant_info != 'No restaurants founded':
+            restaurant = Restaurant(restaurant_name=unicode(restaurant_info['name']),
+                                    restaurant_address=unicode(restaurant_info['address']),
+                                    restaurant_image=unicode(restaurant_info['image']))
+            session.add(restaurant)
+            session.commit()
+            return jsonify(restaurant=restaurant.serialize)
+
+        else:
+            return jsonify({"error": "No Restaurants Found for %s in %s" % (meal_type, location)})
 
 
 @app.route('/restaurants/<int:id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
