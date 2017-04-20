6992dcba1f7d9b57dc765efb47d402a79428ba33
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-04-20 11:09:50 +0600

    Add api methods (GET, POST, PUT, DELETE) for meet request(-s)

--- meet-and-eat/views.py
+++ meet-and-eat/views.py
@@ -35,13 +35,61 @@ def user(id):
 
 
 @app.route('/api/v1/requests/', methods=['POST', 'GET'], strict_slashes=False)
-def requests():
-    pass
+def meet_requests_handler(user_id):
+    if request.method == 'GET':
+        meet_requests = session.query(Request).all()
+        return jsonify(bagels=[i.serialize for i in meet_requests])
+    elif request.method == 'POST':
+        meal_type = request.json.get('meal_type')
+        location_string = request.json.get('location_string')
+        latitude = request.json.get('latitude')
+        longitude = request.json.get('longitude')
+        meal_time = request.json.get('meal_time')
+        filled = request.json.get('filled')
+
+        if session.query(Request).filter_by(user_id=user_id, meal_time=meal_time).first() is not None:
+            print "You already have a meet request in that time, please choose another one"
+            return jsonify({'message': 'You already have a meet request in that time, please choose another time'}), 200
+
+        new_meet_request = Request(meal_type=meal_type, location_string=location_string, latitude=latitude,
+                                   longitude=longitude, meal_time=meal_time, filled=filled, user_id=user_id)
+        session.add(new_meet_request)
+        session.commit()
+        return jsonify(meet_request=new_meet_request.serialize)
 
 
-@app.route('/api/v1/requests/<int:id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def request(id):
-    pass
+@app.route('/api/v1/requests/<int:request_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def meet_request_handler(user_id, request_id):
+    meet_request = session.query(Request).filter_by(request_id=request_id).one()
+    if request.method == 'GET':
+        return jsonify(meet_request=meet_request.serialize)
+    elif request.method == 'PUT':
+        meal_type = request.json.get('meal_type')
+        location_string = request.json.get('location_string')
+        latitude = request.json.get('latitude')
+        longitude = request.json.get('longitude')
+        meal_time = request.json.get('meal_time')
+
+        if meal_type:
+            meet_request.meal_type = meal_type
+        if location_string:
+            meet_request.location_string = location_string
+        if latitude:
+            meet_request.latitude = latitude
+        if longitude:
+            meet_request.longitude = longitude
+        if meal_time:
+            meet_request.meal_time = meal_time
+
+        session.add(meet_request)
+        session.commit()
+
+        return jsonify(meet_request=meet_request.serialize)
+    elif request.method == 'DELETE':
+        session.delete(meet_request)
+        session.commit()
+        print "Removed the meet request with id %s" % request_id
+        return jsonify({'message': 'The meet request has been successfully removed'}), 200
 
 
 @app.route('/api/v1/proposals/', methods=['POST', 'GET'], strict_slashes=False)
